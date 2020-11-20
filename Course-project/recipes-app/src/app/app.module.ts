d6ffe57ebe9bfb6e20ed0f6ec3c5aa2e3728f8d1
MargaritaMarinova <margarita.tomova@gmail.com>
2020-11-20 14:48:06 +0200

    Add Shopping list Service

--- Course-project/recipes-app/src/app/app.module.ts
+++ Course-project/recipes-app/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -28,9 +29,9 @@ import { DropdownDirective } from './shared/dropdown.directive';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
+    FormsModule,
+    ],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
